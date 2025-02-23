78686a5fd5862eb6159ac17fdc9cb1adba7b35d1
sparshkr24 <sparsh24112002@gmail.com>
2025-02-23 22:33:09 +0530

    added name and parallax effect on it

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import "./parallax.scss";
 import { motion, useScroll, useTransform } from "framer-motion";
 
-const Parallax = ({ type }) => {
+const Parallax = () => {
   const ref = useRef();
 
   const { scrollYProgress } = useScroll({
@@ -10,7 +10,7 @@ const Parallax = ({ type }) => {
     offset: ["start start", "end start"],
   });
 
-  const yText = useTransform(scrollYProgress, [0, 1], ["0%", "500%"]);
+  const yText = useTransform(scrollYProgress, [0, 1], ["0%", "300%"]);
   const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
 
   return (
@@ -19,22 +19,22 @@ const Parallax = ({ type }) => {
       ref={ref}
       style={{
         background:
-          type === "services"
+          false
             ? "linear-gradient(180deg, #111132, #0c0c1d)"
             : "linear-gradient(180deg, #111132, #505064)",
       }}
     >
       <motion.h1 style={{ y: yText }}>
-        {type === "services" ? "What We Do?" : "What We Did?"}
+        Hi, I'm Sparsh
+        <br />
+        A software engineer
       </motion.h1>
       <motion.div className="mountains"></motion.div>
       <motion.div
         className="planets"
         style={{
           y: yBg,
-          backgroundImage: `url(${
-            type === "services" ? "/planets.png" : "/sun.png"
-          })`,
+          backgroundImage: `url(/planets.png)`,
         }}
       ></motion.div>
       <motion.div style={{ x: yBg }} className="stars"></motion.div>
