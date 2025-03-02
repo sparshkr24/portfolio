97e7368ce1bb7cfc07fc6e4d045785d2e5ccbbef
sparshkr24 <sparsh24112002@gmail.com>
2025-03-02 22:32:25 +0530

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -20,9 +20,7 @@ const App = () => {
         </div>
         <About />
         <Experience />
-        {/* <Tech /> */}
         <Works />
-        {/* <Feedbacks /> */}
         <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
